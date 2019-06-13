5ccddef302d4bdff2167ab52edea622f0191ce15
Allen Goodman <allen.goodman@icloud.com>
2019-06-13 17:08:33 -0400

    import cleanup

---
+++ src/components/Drawer/ApplicationList/OpenListItem/index.ts
@@ -0,0 +1,12 @@
+export {
+  OpenClassifierMenuItem
+} from './OpenClassifierMenuItem/OpenClassifierMenuItem';
+export {
+  OpenExampleClassifierListItem
+} from './OpenExampleClassifierListItem/OpenExampleClassifierListItem';
+export {
+  OpenExampleClassifierMenuItem
+} from './OpenExampleClassifierMenuItem/OpenExampleClassifierMenuItem';
+export { OpenListItem } from './OpenListItem/OpenListItem';
+export { OpenMenuList } from './OpenMenuList/OpenMenuList';
+export { OpenWeightsMenuItem } from './OpenWeightsMenuItem/OpenWeightsMenuItem';
--- src/components/Drawer/ApplicationList/index.ts
+++ src/components/Drawer/ApplicationList/index.ts
@@ -2,20 +2,13 @@ export { ApplicationList } from './ApplicationList/ApplicationList';
 export {
   NewClassifierListItem
 } from './NewClassifierListItem/NewClassifierListItem';
-export {
-  OpenClassifierMenuItem
-} from './OpenListItem/OpenClassifierMenuItem/OpenClassifierMenuItem';
-export {
-  OpenExampleClassifierListItem
-} from './OpenListItem/OpenExampleClassifierListItem/OpenExampleClassifierListItem';
-export {
-  OpenExampleClassifierMenuItem
-} from './OpenListItem/OpenExampleClassifierMenuItem/OpenExampleClassifierMenuItem';
-export { OpenListItem } from './OpenListItem/OpenListItem/OpenListItem';
-export { OpenMenuList } from './OpenListItem/OpenMenuList/OpenMenuList';
-export {
-  OpenWeightsMenuItem
-} from './OpenListItem/OpenWeightsMenuItem/OpenWeightsMenuItem';
+export { OpenClassifierMenuItem } from './OpenListItem';
+export { OpenExampleClassifierListItem } from './OpenListItem';
+export { OpenExampleClassifierMenuItem } from './OpenListItem';
+export { OpenListItem } from './OpenListItem';
+export { OpenMenuList } from './OpenListItem';
+export { OpenWeightsMenuItem } from './OpenListItem';
+
 export { SaveAnnotationsAndPredictionsMenuItem } from './SaveListItem';
 export { SaveClassifierMenuItem } from './SaveListItem';
 export { SaveListItem } from './SaveListItem';
