d23ebc35236a3f3db632a62a8b12bbbf6061b7a0
Allen Goodman <allen.goodman@icloud.com>
2019-06-14 17:10:02 -0400

    storybook cleanup

--- src/Drawer/Drawer/Drawer.stories.tsx
+++ src/Drawer/Drawer/Drawer.stories.tsx
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react';
 import { Drawer } from './Drawer';
 import {ThemeProvider} from "@material-ui/styles";
 import {createMuiTheme} from "@material-ui/core";
+import { store } from '@piximi/store';
+import { Provider } from "react-redux";
+import { DragDropContextProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend'
 
 const theme = createMuiTheme({
   palette: {
@@ -14,7 +18,11 @@ const toggle = () => {};
 
 storiesOf('Drawer', module)
   .add('example', () => (
-    <ThemeProvider theme={theme}>
-      <Drawer toggle={toggle} toggled/>
-    </ThemeProvider>
+    <DragDropContextProvider backend={HTML5Backend}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Drawer toggle={toggle} toggled/>
+        </ThemeProvider>
+      </Provider>
+    </DragDropContextProvider>
   ));
