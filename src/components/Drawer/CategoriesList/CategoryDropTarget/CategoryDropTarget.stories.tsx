f8fd238adbb73fdaa1a8194c2321b3557f3cb31d
Allen Goodman <allen.goodman@icloud.com>
2019-06-14 16:06:20 -0400

    storybook fixes

--- src/components/Drawer/CategoriesList/CategoryDropTarget/CategoryDropTarget.stories.tsx
+++ src/components/Drawer/CategoriesList/CategoryDropTarget/CategoryDropTarget.stories.tsx
@@ -1,16 +1,23 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import {CategoryDropTarget} from "./CategoryDropTarget";
+import {CategoryListItem} from "../CategoryListItem/CategoryListItem";
 
 const category = {
-  color: '#F44336',
   description: 'example',
   identifier: '11111111-1111-1111-1111-11111111111',
   index: 1,
-  visible: true
+  visible: true,
+  visualization: {
+    color: '#F44336',
+    visible: true
+  }
 };
 
 const updateImageCategory = () => {};
 
 storiesOf('CategoryDropTarget', module).add('example', () => (
-  <div/>
+  <CategoryDropTarget category={category} updateImageCategory={updateImageCategory}>
+    <CategoryListItem/>
+  </CategoryDropTarget>
 ));
